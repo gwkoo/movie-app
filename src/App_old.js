f004f51fc72582861736c6d633f0a90bc24996f4
cmhui <cmhui@grovesoft.net>
2019-07-25 18:48:35 +0900

    toy project - movieApp step2

--- src/App_old.js
+++ src/App_old.js
@@ -40,57 +40,66 @@ class App extends Component {
 
 	// 해당 싸이클은 컴포넌트가 존재하기 시작할 때 자동으로 발생 (1)componentWillMount() -> (2)render() -> (3)componentDidMount
 
-	state = {}
+	state = {
+		greeting: 'Hello!',
+		movies: [
+			{
+				title: "Matrix",
+				poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
+			},
+			{
+				title: "Full Metal Jacket",
+				poster: "https://i.ebayimg.com/images/g/9U4AAMXQfvlSj-e-/s-l300.jpg"
+			},
+			{
+				title: "Oldboy",
+				poster: "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/Oldboy-poster-alternate-by-Rope-of-Silicon.jpg"
+			},
+			{
+				title: "Star Wars",
+				poster: "https://images-na.ssl-images-amazon.com/images/I/51poKKV63GL.jpg"
+			}
+		]
+	}
 
 	componentWillMount(){
 		console.log('will mount')
-	}
-
-	_renderMovies = () => {
-		const movies = this.state.movies.map((movie, index) => {
-			return <Movie title={movie.title} poster={movie.poster} key={index} />
-		})
-		return movies
 	}
 
 	render(){
 		console.log('did render')
 	  return(
 	    <div className="App">
-				{this.state.movies ? this._renderMovies() : 'Loading'}
+				// {this.state.greeting}
+				{this.state.movies.map((movie, index) => { //state에 있던 movies를 function으로 이동했기때문에 this.state.movies가 없으므로 map이 안돌아감
+					return(
+						<Movie title={movie.title} poster={movie.poster} key={index} />
+					)
+					/* 상위 맵핑과 동일한 코드 (map에는 array 기능이 있다)
+					{(
+						<Movie title={movies[0].title} poster={movies[0].poster} />
+						<Movie title={movies[1].title} poster={movies[1].poster} />
+						<Movie title={movies[2].title} poster={movies[2].poster} />
+						<Movie title={movies[3].title} poster={movies[3].poster} />
+					)}*/
+				})}
 	    </div>
 	  );
 	}
 
 	componentDidMount(){
 		console.log('did mount');
-		/*setTimeout(() => {
+		setTimeout(() => {
 			this.setState({
 				movies: [
+					//...this.state.movies, // '이전 영화 리스트를 그대로 두고,  한 개 영화를 추가' 라는 뜻
 					{
-						title: "Matrix",
-						poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
-					},
-					{
-						title: "Full Metal Jacket",
-						poster: "https://i.ebayimg.com/images/g/9U4AAMXQfvlSj-e-/s-l300.jpg"
-					},
-					{
-						title: "Oldboy",
-						poster: "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/Oldboy-poster-alternate-by-Rope-of-Silicon.jpg"
-					},
-					{
-						title: "Star Wars",
+						title: "Trainspotting",
 						poster: "https://images-na.ssl-images-amazon.com/images/I/51poKKV63GL.jpg"
 					}
 				]
 			})
-		}, 5000)*/
-		// promise는 성공적으로 수행할 수 있고, 그렇지 않을 경우 결과물을 catch, then으로 받아볼 수 있음
-		fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating') // fetch는 심플하게 url을 ajax로 불러올 수 있음 (ver.Old : xml http request)
-		.then(response => response.json())
-		.then(json => console.log(json))
-		.catch(err => console.log(err))
+		}, 5000)
 	}
 
 }
---
+++ src/Movie_old.js
@@ -0,0 +1,63 @@
+import React, from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+class Movie extends Component{
+
+	static propTypes = {
+		title: PropTypes.string.isRequired, // isRequired는 해당 요소가 필수임을 나타냄
+		poster: PropTypes.string.isRequired
+	}
+
+	render(){
+		//console.log(this.props);
+		return(
+			<div>
+				<MoviePoster poster={this.props.poster} />
+				<h1>{this.props.title}</h1>
+			</div>
+		)
+	}
+}
+
+class MoviePoster extends Component{
+
+	static propTypes = {
+		poster: PropTypes.string.isRequired
+	}
+
+	render(){
+		//console.log(this.props)
+		return(
+			<img src={this.props.poster} alt="Movie Poster"/>
+		)
+	}
+}
+
+/* 해당 function 컴포넌트는 상기 class 컴포넌트와 같다.
+functional 컴포넌트(dumb 컴포넌트)는 한 개의 props와 한 개의 html태그만 필요 (state가 없음)*/
+function Movie({title, poster}){
+	return (
+		<div>
+			<MoviePoster poster={poster} />
+			<h1>{title}</h1>
+		</div>
+	)
+}
+
+function MoviePoster({poster}){
+	return (
+		<img src={ poster} alt="Movie Poster"/>
+	)
+}
+
+Movie.propTypes = {
+	title: propTypes.string.isRequired,
+	poster: propTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+	poster: propTypes.string.isRequired
+}
+
+export default Movie;
